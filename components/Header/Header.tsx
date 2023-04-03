76a1a03d8691ff7b8b2714e5e6a8ab28c93c6c4f
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-04-03 20:55:36 +0300

    adding header right side

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Image from "next/image";
 import {BiSearch} from "react-icons/bi";
+import {AiFillHome} from "react-icons/ai";
+import {BsPlusCircle} from 'react-icons/bs';
 
 const Header = () => {
 	return (
 		<div className="">
 			<div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
 				{/* LEFT */}
-				<div className=" h-24 w-24 relative hidden lg:inline-grid cursor-pointer mt-10">
+				<div className=" h-36 w-36 relative hidden lg:inline-grid cursor-pointer mt-16">
 					<Image
-						width={100}
-						height={100}
+						width={103}
+						height={29}
 						src={'https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram-1.png'}
 						alt={'Insta Logo'}
 						className="object-contain"
@@ -37,7 +39,11 @@ const Header = () => {
 				</div>
 
 				{/* RIGHT */}
-				<h1>Right Side</h1>
+
+				<div>
+					<AiFillHome className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"/>
+					<BsPlusCircle className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"/>
+				</div>
 			</div>
 
 
