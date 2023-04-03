75795398e88deea661e03df0d0d73c913f6b58a5
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-04-03 20:34:13 +0300

    adding header search bar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import Image from "next/image";
+import {BiSearch} from "react-icons/bi";
+
 const Header = () => {
 	return (
 		<div className="">
-			<div className="flex items-center justify-between max-w-6xl">
+			<div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
 				{/* LEFT */}
-				<div className=" h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+				<div className=" h-24 w-24 relative hidden lg:inline-grid cursor-pointer mt-10">
 					<Image
 						width={100}
-						height={200}
+						height={100}
 						src={'https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram-1.png'}
 						alt={'Insta Logo'}
 						className="object-contain"
 					/>
 				</div>
-				<div className=" h-24 w-10 relative lg:hidden cursor-pointer">
+				<div className=" h-24 w-10 relative lg:hidden cursor-pointer  mt-14">
 					<Image
 						width={100}
 						height={200}
@@ -24,6 +26,15 @@ const Header = () => {
 					/>
 				</div>
 				{/* MIDDLE */}
+				<div className="relative mt-1">
+					<div className="absolute top-2 left-2">
+						<BiSearch className="h-5 text-gray-500 "/>
+					</div>
+					<input
+						type="text"
+						className="rounded-md bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black"
+						placeholder="Search"/>
+				</div>
 
 				{/* RIGHT */}
 				<h1>Right Side</h1>
